7e12671fc79eed7e36e8c2cc61470eb131fe1639
brhanegiday <bgiday@andrew.cmu.edu>
2025-02-22 17:31:31 +0200

    Add prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -7,21 +7,3 @@ const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
-
-// import { PrismaClient } from "@prisma/client";
-
-// let prisma: PrismaClient;
-
-// if (process.env.NODE_ENV === "production") {
-//     prisma = new PrismaClient();
-// } else {
-//     // Ensure we don't create multiple instances in development
-//     if (!(global as any).prisma) {
-//         (global as any).prisma = new PrismaClient({
-//             log: ["query", "error", "warn"],
-//         });
-//     }
-//     prisma = (global as any).prisma;
-// }
-
-// export default prisma;
